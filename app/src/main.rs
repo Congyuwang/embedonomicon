7541d0da4dc904c970a015a61ecd05858750ed09
Congyu WANG <leonwang998@gmail.com>
2024-06-19 15:31:09 +0800

    log with Symbol

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,14 +1,37 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-rt::entry!(main);
+use cortex_m_semihosting::{
+    debug,
+    hio::{self, HostStream},
+};
 
-fn main() -> ! {
-    loop {}
+use log::{error, warn, Log};
+use rt::entry;
+
+struct Logger {
+    hstdout: HostStream,
 }
 
-#[allow(non_snake_case)]
-#[no_mangle]
-pub fn HardFault(_ef: *const u32) -> ! {
+impl Log for Logger {
+    type Error = ();
+
+    fn log(&mut self, address: u8) -> Result<(), ()> {
+        self.hstdout.write_all(&[address])
+    }
+}
+
+entry!(main);
+
+fn main() -> ! {
+    let hstdout = hio::hstdout().unwrap();
+    let mut logger = Logger { hstdout };
+
+    let _ = warn!(logger, "Hello, world!");
+
+    let _ = error!(logger, "Goodbye");
+
+    debug::exit(debug::EXIT_SUCCESS);
+
     loop {}
 }
